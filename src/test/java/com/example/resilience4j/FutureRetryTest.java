bac59d990fcd3add673cd90074b643e0f5090dea
kuanghc1 <kuanghc1@lenovo.com>
2022-07-20 11:39:27 +0800

    Reformat code

--- src/test/java/com/example/resilience4j/FutureRetryTest.java
+++ src/test/java/com/example/resilience4j/FutureRetryTest.java
@@ -8,48 +8,48 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class FutureRetryTest extends AbstractRetryTest {
 
-	@Test
-	public void backendAshouldRetryThreeTimes() {
-		// When
-		float currentCount = getCurrentCount(FAILED_WITH_RETRY, BACKEND_A);
-		produceFailure(BACKEND_A);
+    @Test
+    public void backendAshouldRetryThreeTimes() {
+        // When
+        float currentCount = getCurrentCount(FAILED_WITH_RETRY, BACKEND_A);
+        produceFailure(BACKEND_A);
 
-		checkMetrics(FAILED_WITH_RETRY, BACKEND_A, currentCount + 1);
-	}
+        checkMetrics(FAILED_WITH_RETRY, BACKEND_A, currentCount + 1);
+    }
 
-	@Test
-	public void backendBshouldRetryThreeTimes() {
-		// When
-		float currentCount = getCurrentCount(FAILED_WITH_RETRY, BACKEND_B);
-		produceFailure(BACKEND_B);
+    @Test
+    public void backendBshouldRetryThreeTimes() {
+        // When
+        float currentCount = getCurrentCount(FAILED_WITH_RETRY, BACKEND_B);
+        produceFailure(BACKEND_B);
 
-		checkMetrics(FAILED_WITH_RETRY, BACKEND_B, currentCount + 1);
-	}
+        checkMetrics(FAILED_WITH_RETRY, BACKEND_B, currentCount + 1);
+    }
 
-	@Test
-	public void backendAshouldSucceedWithoutRetry() {
-		float currentCount = getCurrentCount(SUCCESS_WITHOUT_RETRY, BACKEND_A);
-		produceSuccess(BACKEND_A);
+    @Test
+    public void backendAshouldSucceedWithoutRetry() {
+        float currentCount = getCurrentCount(SUCCESS_WITHOUT_RETRY, BACKEND_A);
+        produceSuccess(BACKEND_A);
 
-		checkMetrics(SUCCESS_WITHOUT_RETRY, BACKEND_A, currentCount + 1);
-	}
+        checkMetrics(SUCCESS_WITHOUT_RETRY, BACKEND_A, currentCount + 1);
+    }
 
-	@Test
-	public void backendBshouldSucceedWithoutRetry() {
-		float currentCount = getCurrentCount(SUCCESS_WITHOUT_RETRY, BACKEND_B);
-		produceSuccess(BACKEND_B);
+    @Test
+    public void backendBshouldSucceedWithoutRetry() {
+        float currentCount = getCurrentCount(SUCCESS_WITHOUT_RETRY, BACKEND_B);
+        produceSuccess(BACKEND_B);
 
-		checkMetrics(SUCCESS_WITHOUT_RETRY, BACKEND_B, currentCount + 1);
-	}
+        checkMetrics(SUCCESS_WITHOUT_RETRY, BACKEND_B, currentCount + 1);
+    }
 
-	private void produceFailure(String backend) {
-		ResponseEntity<String> response = restTemplate.getForEntity("/" + backend + "/futureFailure", String.class);
-		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
-	}
+    private void produceFailure(String backend) {
+        ResponseEntity<String> response = restTemplate.getForEntity("/" + backend + "/futureFailure", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
-	private void produceSuccess(String backend) {
-		ResponseEntity<String> response = restTemplate.getForEntity("/" + backend + "/futureSuccess", String.class);
-		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-	}
+    private void produceSuccess(String backend) {
+        ResponseEntity<String> response = restTemplate.getForEntity("/" + backend + "/futureSuccess", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
 
 }
